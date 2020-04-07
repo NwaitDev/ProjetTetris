0e53785795d73fd71770ea342610f4b5d390e539
NwaitDev <nathangallone@hotmail.com>
2020-04-07 23:40:33 +0200

    ROTATION/CanvaSize/jsDoc/colors/modifConstr

--- script.js
+++ script.js
@@ -40,60 +40,198 @@ var rightKeyDown = false;
 
 /**
  * Constructeur du type Block
- * @param {abscisse} x 
- * @param {ordonnée} y 
+ * @param {number} x 
+ * @param {number} y 
  */
-var Block = function(x,y){
+var Block = function (x, y) {
     this.x = x;
     this.y = y;
     var self = this;
     /**
      * décalage d'un bloc selon x et y
+     * @param {int} xShift décalage du bloc selon l'abscisse
+     * @param {int} yShift décalage du bloc selon l'ordonnée
      */
-    this.move = function(xShift,yShift){
-        self.x+=xShift;
-        self.y+=yShift;
+    this.move = function (xShift, yShift) {
+        self.x += xShift;
+        self.y += yShift;
     }
 }
+
 /**
  * Constructeur du type Tetromino
- * @param {premier_bloc_du_tetromino} block1 
- * @param {deuxième_bloc_du_tetromino} block2 
- * @param {troisième_bloc_du_tetromino} block3 
- * @param {quatrième_bloc_du_tetromino} block4 
- * @param {couleur_du_tetromino} color 
+ * @param {Block} block1 premier bloc 
+ * @param {Block} block2 deuxième bloc
+ * @param {Block} block3 troisième bloc
+ * @param {Block} block4 quatrième bloc
+ * @param {number} color identifiant de la couleur du tetromino (entier entre 1 et 7)
+ * @param {number} nbRot nombre de rotation du tetromino %4
  */
-var Tetromino = function Tetromino(block1,block2,block3,block4,color){
+var Tetromino = function Tetromino(block1, block2, block3, block4, color, nbRot) {
     this.block1 = block1;
     this.block2 = block2;
     this.block3 = block3;
     this.block4 = block4;
     this.color = color;
+    this.nbRot = nbRot;
     var self = this;
     /**
      * Vérification de la position du tetromino
+     * @returns {boolean} true si le tetromino est dans la grille et non-superposé à un bloc
      */
-    this.check = function(){
-        let res = (grid[self.block1.x]!==undefined)&&
-        (grid[self.block2.x]!==undefined)&&
-        (grid[self.block3.x]!==undefined)&&
-        (grid[self.block4.x]!==undefined);
-        if (res){
-            res = (grid[self.block1.x][self.block1.y]==0)&&
-            (grid[self.block2.x][self.block2.y]==0)&&
-            (grid[self.block3.x][self.block3.y]==0)&&
-            (grid[self.block4.x][self.block4.y]==0);
+    this.check = function () {
+        let res = (grid[self.block1.x] !== undefined) &&
+            (grid[self.block2.x] !== undefined) &&
+            (grid[self.block3.x] !== undefined) &&
+            (grid[self.block4.x] !== undefined);
+        if (res) {
+            res = (grid[self.block1.x][self.block1.y] == 0) &&
+                (grid[self.block2.x][self.block2.y] == 0) &&
+                (grid[self.block3.x][self.block3.y] == 0) &&
+                (grid[self.block4.x][self.block4.y] == 0);
         }
         return res;
     }
+    /**
+     * renvoie un nouvel objet de type Tetromino de mêmes propriétés que le tetromino
+     */
+    this.copy = function () {
+        return new Tetromino(
+            new Block(self.block1.x, self.block1.y),
+            new Block(self.block2.x, self.block2.y),
+            new Block(self.block3.x, self.block3.y),
+            new Block(self.block4.x, self.block4.y),
+            self.color,
+            self.nbRot
+        );
+    }
+
+    /**
+     * chute du tetromino
+     */
+    this.fall = function () {
+        self.block1.y += 1;
+        self.block2.y += 1;
+        self.block3.y += 1;
+        self.block4.y += 1;
+    }
     
+    /**
+     * décalage à gauche du tetromino
+     */
+    this.leftShift = function () {
+        self.block1.x -= 1;
+        self.block2.x -= 1;
+        self.block3.x -= 1;
+        self.block4.x -= 1;
+    }
+
+    /**
+     * décalage à droite du tetromino
+     */
+    this.rightShift = function () {
+        self.block1.x += 1;
+        self.block2.x += 1;
+        self.block3.x += 1;
+        self.block4.x += 1;
+    }
+
+    /**
+     * décalage vers le haut du tetromino
+     */
+    this.upShift = function () {
+        self.block1.y -= 1;
+        self.block2.y -= 1;
+        self.block3.y -= 1;
+        self.block4.y -= 1;
+    }
 }
 
+/**
+ * remplace le tetromino en cours de placement par un autre tetromino en haut de la grille
+ * @returns {Tetromino} un nouveau tetromino en haut de la grille
+ */
+spawnTetromino = function(){
+    let tetromino;
+    switch(nextColor){
+        case 1 :
+            tetromino = new Tetromino(
+                new Block(4,0),
+                new Block(3,0),
+                new Block(5,0),
+                new Block(6,0),
+                1,
+                0
+            );
+        break;
+        case 2 :
+            tetromino = new Tetromino(
+                new Block(4,1),
+                new Block(3,1),
+                new Block(3,0),
+                new Block(5,1),
+                2,
+                0
+            );
+        break;
+        case 3 :
+            tetromino = new Tetromino(
+                new Block(4,1),
+                new Block(5,1),
+                new Block(5,0),
+                new Block(3,1),
+                3,
+                0
+            );
+        break;
+        case 4 :
+            tetromino = new Tetromino(
+                new Block(4,0),
+                new Block(5,0),
+                new Block(4,1),
+                new Block(5,1),
+                4,
+                0
+            );
+        break;
+        case 5 :
+            tetromino = new Tetromino(
+                new Block(4,1),
+                new Block(3,1),
+                new Block(4,0),
+                new Block(5,0),
+                5,
+                0
+            );
+        break;
+        case 6 :
+            tetromino = new Tetromino(
+                new Block(4,1),
+                new Block(5,1),
+                new Block(4,0),
+                new Block(3,1),
+                6,
+                0
+            );
+        break;
+        case 7 :
+            tetromino = new Tetromino(
+                new Block(4,1),
+                new Block(5,1),
+                new Block(4,0),
+                new Block(3,0),
+                7,
+                0
+            );
+    }
+    nextColor = Math.ceil(Math.random()*7);
+    return tetromino;
+}
 
 /**
  * Fonction d'initialisation/lancement du jeu
  */
-init = function init() {
+init = function() {
     context = document.getElementById("cvs").getContext("2d");
     context.width = document.getElementById("cvs").width;
     context.height = document.getElementById("cvs").height;
@@ -101,7 +239,7 @@ init = function init() {
     document.addEventListener("keyup", relacheClavier);
     tileWidth = context.width/10;
     tileHeight = context.height/20;
-    spawnTetromino();
+    tetromino = spawnTetromino();
     runGame();
 }
 /**
@@ -137,89 +275,63 @@ printStuff = function printStuff() {
     }
 }
 
+/**
+ * retourne le code Hexadécimal de la couleur d'un tetromino
+ * @param {number} color Couleur du tetromino
+ * @returns {String} Couleur en héxadécimale
+ */
 color = function color(color){
     switch (color){
         case 1 :
-            return "#11F";
-        case 2 :
-            return "#F0F";
-        case 3 :
-            return "#FA5";
-        case 4 :
-            return "#11A";
-        case 5 :
-            return "#FF0";
-        case 6 :
-            return "F40";
-        case 7 :
             return "#FFF";
+        case 2 :
+            return "#FF0";
+        case 3 :
+            return "#F0F";
+        case 4 :
+            return "#0FF";
+        case 5 :
+            return "#F00";
+        case 6 :
+            return "#0F0";
+        case 7 :
+            return "#00F";
     }
 }
 
 /**
  * Fonction de mise à jour de l'état du jeu
- * @param {Date} d 
+ * @param {number} d date 
  */
 update = function update(d) {
     if(d-lastTimeUpdate > 700 || (downKeyDown && d-lastTimeUpdate > 100)){
-        fall();
+        tetromino.fall();
         if(!tetromino.check()){
-            upShift();
+            tetromino.upShift();
             printInGrid();
-            spawnTetromino();
+            tetromino = spawnTetromino();
         }
         lastTimeUpdate = d;
     }
     if(rightKeyDown && d-lastMoveTime > 100){
-        rightShift();
+        tetromino.rightShift();
         if(!tetromino.check()){
-            leftShift();
+            tetromino.leftShift();
         }
         lastMoveTime = d;
     }
     if(leftKeyDown && d-lastMoveTime > 100){
-        leftShift();
+        tetromino.leftShift();
         if(!tetromino.check()){
-            rightShift();
+            tetromino.rightShift();
         }
         lastMoveTime = d;
     }
+    if(upKeyDown && d-lastMoveTime > 100){
+        tetromino = rotate(tetromino);
+        lastMoveTime = d;
+    }
 }
-
-/*
- * Fonctions de déplacement du tetromino
- */
-
-////////////////////////////
-fall = function(){
-    tetromino.block1.y+=1;
-    tetromino.block2.y+=1;
-    tetromino.block3.y+=1;
-    tetromino.block4.y+=1;
-}
-
-leftShift = function(){
-    tetromino.block1.x-=1;
-    tetromino.block2.x-=1;
-    tetromino.block3.x-=1;
-    tetromino.block4.x-=1;
-}
-
-rightShift = function(){
-    tetromino.block1.x+=1;
-    tetromino.block2.x+=1;
-    tetromino.block3.x+=1;
-    tetromino.block4.x+=1;
-}
-
-upShift = function(){
-    tetromino.block1.y-=1;
-    tetromino.block2.y-=1;
-    tetromino.block3.y-=1;
-    tetromino.block4.y-=1;
-}
-
-
 
 /////////////////////////////
 
@@ -234,74 +346,227 @@ printInGrid = function printInGrid(){
 }
 
 /**
- * remplace le tetromino en cours de placement par un autre tetromino en haut de la grille
+ * retourne la version tournée d'un quart de tour du tetromino en paramètre
+ * @param {Tetromino} tetromino 
+ * @returns {Tetromino} la version tournée d'un quart de tour de tetromino (si possible)
  */
-spawnTetromino = function spawnTetromino(){
-    switch(nextColor){
-        case 1 :
-            tetromino = new Tetromino(
-                new Block(4,0),
-                new Block(3,0),
-                new Block(5,0),
-                new Block(6,0),
-                1
-            );
-        break;
-        case 2 :
-            tetromino = new Tetromino(
-                new Block(4,1),
-                new Block(3,1),
-                new Block(3,0),
-                new Block(5,1),
-                2
-            );
-        break;
-        case 3 :
-            tetromino = new Tetromino(
-                new Block(4,1),
-                new Block(5,1),
-                new Block(5,0),
-                new Block(3,1),
-                3
-            );
-        break;
-        case 4 :
-            tetromino = new Tetromino(
-                new Block(4,0),
-                new Block(5,0),
-                new Block(4,1),
-                new Block(5,1),
-                4
-            );
-        break;
-        case 5 :
-            tetromino = new Tetromino(
-                new Block(4,1),
-                new Block(3,1),
-                new Block(4,0),
-                new Block(5,0),
-                5
-            );
-        break;
-        case 6 :
-            tetromino = new Tetromino(
-                new Block(4,1),
-                new Block(5,1),
-                new Block(4,0),
-                new Block(3,1),
-                6
-            );
-        break;
-        case 7 :
-            tetromino = new Tetromino(
-                new Block(4,1),
-                new Block(5,1),
-                new Block(4,0),
-                new Block(3,0),
-                7
-            );
+rotate = function (tetromino) {
+
+    let res = tetromino.copy();
+
+    switch (res.color) {
+        case 1:
+            switch (res.nbRot) {
+                case 0:
+                    res.block1.move(1, 0);
+                    res.block2.move(2, -1);
+                    res.block3.move(0, 1);
+                    res.block4.move(-1, 2);
+                    res.nbRot++;
+                    break;
+                case 1:
+                    res.block1.move(0, 1);
+                    res.block2.move(1, 2);
+                    res.block3.move(-1, 0);
+                    res.block4.move(-2, -1);
+                    res.nbRot++;
+                    break;
+                case 2:
+                    res.block1.move(-1, 0);
+                    res.block2.move(-2, 1);
+                    res.block3.move(0, -1);
+                    res.block4.move(1, -2);
+                    res.nbRot++;
+                    break;
+                case 3:
+                    res.block1.move(0, -1);
+                    res.block2.move(-1, -2);
+                    res.block3.move(1, 0);
+                    res.block4.move(2, 1);
+                    res.nbRot=0;
+                    break;
+            }
+            break;
+
+        case 2:
+            switch (res.nbRot) {
+                case 0:
+                    res.block2.move(1, -1);
+                    res.block3.move(2, 0);
+                    res.block4.move(-1, 1);
+                    res.nbRot++;                  
+                    break;
+                case 1:
+                    res.block2.move(1, 1);
+                    res.block3.move(0, 2);
+                    res.block4.move(-1, -1);
+                    res.nbRot++;
+                    break;
+                case 2:
+                    res.block2.move(-1, 1);
+                    res.block3.move(-2, 0);
+                    res.block4.move(1, -1);
+                    res.nbRot++;
+                    break;
+                case 3:
+                    res.block2.move(-1, -1);
+                    res.block3.move(0, -2);
+                    res.block4.move(1, 1);
+                    res.nbRot = 0;
+            }
+            break;
+
+        case 3:
+            switch (res.nbRot) {
+                case 0:
+                    res.block2.move(-1, 1);
+                    res.block3.move(0, 2);
+                    res.block4.move(1,-1);
+                    res.nbRot++;
+                    break;
+                case 1:
+                    res.block2.move(-1, -1);
+                    res.block3.move(-2, 0);
+                    res.block4.move(1, 1);
+                    res.nbRot++;
+                    break;
+                case 2:
+                    res.block2.move(1, -1);
+                    res.block3.move(0, -2);
+                    res.block4.move(-1, 1);
+                    res.nbRot++;
+                    break;
+                case 3:
+                    res.block2.move(1, 1);
+                    res.block3.move(2, 0);
+                    res.block4.move(-1, -1);
+                    res.nbRot = 0;
+            }
+            break;
+
+        case 5:
+            switch (res.nbRot) {
+                case 0:
+                    res.block2.move(1, -1);
+                    res.block3.move(1, 1);
+                    res.block4.move(0, 2);
+                    res.nbRot++;
+                    break;
+                case 1:
+                    res.block2.move(1, 1);
+                    res.block3.move(-1, 1);
+                    res.block4.move(-2, 0);
+                    res.nbRot++;
+                    break;
+                case 2:
+                    res.block2.move(-1, 1);
+                    res.block3.move(-1, -1);
+                    res.block4.move(0, -2);
+                    res.nbRot++;
+                    break;
+                case 3:
+                    res.block2.move(-1, -1);
+                    res.block3.move(1, -1);
+                    res.block4.move(2, 0);
+                    res.nbRot = 0;
+            }
+            break;
+            
+        case 6:
+            //OK
+            switch (res.nbRot) {
+                case 0:
+                    res.block2.move(-1, 1);
+                    res.block3.move(1, 1);
+                    res.block4.move(1, -1);
+                    res.nbRot++;
+                    break;
+                case 1:
+                    res.block2.move(-1, -1);
+                    res.block3.move(-1, 1);
+                    res.block4.move(1, 1);
+                    res.nbRot++;
+                    break;
+                case 2:
+                    res.block2.move(1, -1);
+                    res.block3.move(-1, -1);
+                    res.block4.move(-1, 1);
+                    res.nbRot++;
+                    break;
+                case 3:
+                    res.block2.move(1, 1);
+                    res.block3.move(1, -1);
+                    res.block4.move(-1, -1);
+                    res.nbRot = 0;
+            }
+            break;
+            
+        case 7:
+            //OK
+            switch (res.nbRot) {
+                case 0:
+                    res.block2.move(-1, 1);
+                    res.block3.move(1, 1);
+                    res.block4.move(2, 0);
+                    res.nbRot++;
+                    break;
+                case 1:
+                    res.block2.move(-1, -1);
+                    res.block3.move(-1, 1);
+                    res.block4.move(0, 2);
+                    res.nbRot++;
+                    break;
+                case 2:
+                    res.block2.move(1, -1);
+                    res.block3.move(-1, -1);
+                    res.block4.move(-2, 0);
+                    res.nbRot++;
+                    break;
+                case 3:
+                    res.block2.move(1, 1);
+                    res.block3.move(1, -1);
+                    res.block4.move(0, -2);
+                    res.nbRot = 0;
+            }
     }
-    nextColor = Math.ceil(Math.random()*7);
+    if (res.check()) {
+        return res;
+    } else {
+        res.leftShift();
+        if(res.check()){
+            return res;
+        }else{
+            if(res.color==1){
+                res.leftShift();
+                if(res.check()){
+                    return res;
+                }else{
+                    res.rightShift();
+                }
+            }
+            res.rightShift();
+            res.rightShift();
+            if(res.check()){
+                return res;
+            }else{
+                if(res.color==1){
+                    res.rightShift();
+                    if(res.check()){
+                        return res;
+                    }else{
+                        res.leftShift();
+                    }
+                }
+                res.leftShift();
+                res.upShift();
+                if(res.check()){
+                    return res;
+                }else{
+                    return tetromino;
+                }
+            }
+        }
+    }
 }
 
 appuiClavier = function appuiClavier(event){
