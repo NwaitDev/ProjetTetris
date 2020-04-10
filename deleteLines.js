cb33489dd4b6c80852afab14deaa91db24cf388f
NwaitDev <nathangallone@hotmail.com>
2020-04-10 18:19:16 +0200

    Ajout algo suppression ligne ;)

--- deleteLines.js
+++ deleteLines.js
@@ -1,97 +1,104 @@
-
-//Reste à faire la descente des lignes (action updateLines du rapport)
-//Je vais m'en occuper
-
 /**
  * Fonction vérifiant si une ligne de la grille est complétée
- * @param {tetromino qui s'est fixé sur la grille} tetromino
- * @param {grille de jeu} grid
+ * @param {Tetromino} tetromino tetromino qui s'est fixé sur la grille
+ * @param {int[][]} grid grille de jeu
+ * @returns {int[]} tableau des cases complétées
  */
-isCompleted = function isCompleted(tetromino, grid){
+completedLines = function(tetromino, grid){
     var isCompleted = true;
-    var res = new Array ();
+    var res = [];
     var i = 0;
-    var j= 0;
     while (isCompleted && i<9){
-        if(grid [tetromino.block1.y][i] !=0){
+        if(grid [i][tetromino.block1.y] !=0){
             i++;
         }else{
             isCompleted = false;
         }
     }
     if(isCompleted){
-        res[j] = tetromino.block1.y;
-        j++;
-    }
-
-    i = 0; 
-    isCompleted = true;
-    while (isCompleted && i<9){
-        if(grid [tetromino.block2.y][i] !=0){
-            i++;
-        }else{
-            isCompleted = false;
-        }
-    }
-    if(isCompleted){
-        res[j] = tetromino.block2.y;
-        j++
-    }
-
-    i = 0; 
-    isCompleted = true;
-    while (isCompleted && i<9){
-        if(grid [tetromino.block3.y][i] !=0){
-            i++;
-        }else{
-            isCompleted = false;
-        }
-    }
-    if(isCompleted){
-        res[j] = tetromino.block3.y;
-        j++;
+        res.push(tetromino.block1.y);
     }
     
-    i = 0; 
-    isCompleted = true;
-    while (isCompleted && i<9){
-        if(grid [tetromino.block4.y][i] !=0){
-            i++;
-        }else{
-            isCompleted = false;
+    if (!res.includes(tetromino.block2.y)){
+        i = 0; 
+        isCompleted = true;
+        while (isCompleted && i<9){
+            if(grid [i][tetromino.block2.y] !=0){
+                i++;
+            }else{
+                isCompleted = false;
+            }
+        }
+        if(isCompleted){
+            res.push(tetromino.block2.y);
         }
     }
-    if(isCompleted){
-        res[j] = tetromino.block4.y;
-        j++;
+
+    if(!res.includes(tetromino.block3.y)){
+        i = 0; 
+        isCompleted = true;
+        while (isCompleted && i<9){
+            if(grid [i][tetromino.block3.y] !=0){
+                i++;
+            }else{
+                isCompleted = false;
+            }
+        }
+        if(isCompleted){
+            res.push(tetromino.block3.y);
+        }
+    }
+    
+    if(!res.includes(tetromino.block4.y))
+        i = 0; 
+        isCompleted = true;
+        while (isCompleted && i<9){
+            if(grid [i][tetromino.block4.y] !=0){
+                i++;
+            }else{
+                isCompleted = false;
+            }
+        }
+        if(isCompleted){
+            res.push(tetromino.block4.y);
+        }
     }
     return res;
 }
 
 /**
  * Action de descente des lignes de la grille de jeu après la suppression d'une lignes
- * @param {ligne supprimée} y
- * @param {grille de jeu} grid
+ * @param {number} y ordonnée de la ligne supprimée
+ * @param {number[][]} grid grille de jeu
  */
- lineFall = function lineFall (y, grid){
-    for (var i = 0; i < 9; i++){
-        grid [y][i] = grid [y-1][i];
-        grid [y-1][i] = 0;
+ lineFall = function(y, grid){
+    for (var i = 0; i < 10; i++){
+        grid [i][y] = grid [i][y-1];
+        grid [i][y-1] = 0;
     }
  }
 
 /**
  * Action de suppression des lignes complétées
- * @param {indices des lignes à supprimer} lines
- * @param {grille de jeu} grid
+ * @param {number[]} lines indices des lignes à supprimer
+ * @param {number[][]} grid grille de jeu
  */
-deleteLines = function deleteLines(lines, grid){
-    for (var i=0; i<lines.length; i++){
-        for (var j=0; j<9; j++){
-            grid[j][lines[i]] = 0;
+deleteLines = function(lines, grid){
+    let y;
+    for (let i=0; i<lines.length; i++){
+        y = lines[i];
+        for (let x=0; j<9; j++){
+            grid[x][y] = 0;
         }
-        for (var k= i; k<1; k++){
-            lineFall(j,grid);
+        for (let k=y; k>0; k--){
+            lineFall(k,grid);
         }
     }
+}
+
+//ordonnée des lignes complétées :
+var lines = new Array();
+lines = completedLines(tetromino, grid);
+if(lines.length !=0){
+    deleteLines(lines,grid);
 }
--- script.js
+++ script.js
@@ -39,6 +39,8 @@ var upKeyDown = false;
 var leftKeyDown = false;
 var rightKeyDown = false;
 
+//ordonnée des lignes complétées :
+var lines = new Array();
 
 /**
  * Constructeur du type Block
@@ -76,7 +78,9 @@ var Tetromino = function Tetromino(block1, block2, block3, block4, color, nbRot)
     this.block4 = block4;
     this.color = color;
     this.nbRot = nbRot;
+
     var self = this;
+
     /**
      * Vérification de la position du tetromino
      * @returns {boolean} true si le tetromino est dans la grille et non-superposé à un bloc
@@ -94,6 +98,7 @@ var Tetromino = function Tetromino(block1, block2, block3, block4, color, nbRot)
         }
         return res;
     }
+
     /**
      * renvoie un nouvel objet de type Tetromino de mêmes propriétés que le tetromino
      */
@@ -231,78 +236,6 @@ spawnTetromino = function(){
 }
 
 /**
- * Fonction d'initialisation/lancement du jeu
- */
-init = function() {
-    context = document.getElementById("cvs").getContext("2d");
-    context.width = document.getElementById("cvs").width;
-    context.height = document.getElementById("cvs").height;
-    document.addEventListener("keydown", appuiClavier);
-    document.addEventListener("keyup", relacheClavier);
-    tileWidth = context.width/10;
-    tileHeight = (context.height-scoreWindowHeight)/20;
-    tetromino = spawnTetromino();
-    runGame();
-}
-/**
- * Boucle de jeu
- */
-runGame = function runGame() {
-    update(Date.now());
-    printStuff();
-    requestAnimationFrame(runGame);
-}
-
-/**
- * Affichage des éléments suivants : 
- * - grille de jeu (à finir)
- * - tetromino en cours de placement
- * - scores (à faire)
- */
-printStuff = function printStuff() {
-    context.clearRect(0, 0, context.width, context.height);
-    
-    //affichage de la grille
-    context.fillStyle = "#555";
-    for(let i = 1; i<10;i++){
-        context.fillRect(tileWidth*i-1,0,2,context.height);
-    }
-    for(let i = 1;i<20;i++){
-        context.fillRect(0,tileHeight*i-1,context.width,2);
-    }
-
-    //affichage du tetromino
-    context.fillStyle = "#FFF"
-    context.fillRect(tetromino.block1.x*tileWidth, tetromino.block1.y*tileHeight, tileWidth, tileHeight);
-    context.fillRect(tetromino.block2.x*tileWidth, tetromino.block2.y*tileHeight, tileWidth, tileHeight);
-    context.fillRect(tetromino.block3.x*tileWidth, tetromino.block3.y*tileHeight, tileWidth, tileHeight);
-    context.fillRect(tetromino.block4.x*tileWidth, tetromino.block4.y*tileHeight, tileWidth, tileHeight);
-    context.fillStyle = color(tetromino.color);
-    context.fillRect(tetromino.block1.x*tileWidth+1, tetromino.block1.y*tileHeight+1, tileWidth-2, tileHeight-2);
-    context.fillRect(tetromino.block2.x*tileWidth+1, tetromino.block2.y*tileHeight+1, tileWidth-2, tileHeight-2);
-    context.fillRect(tetromino.block3.x*tileWidth+1, tetromino.block3.y*tileHeight+1, tileWidth-2, tileHeight-2);
-    context.fillRect(tetromino.block4.x*tileWidth+1, tetromino.block4.y*tileHeight+1, tileWidth-2, tileHeight-2);
-
-    //affichage du score
-    context.fillStyle = "#B0B";
-    context.fillRect(0,context.height-scoreWindowHeight,context.width,scoreWindowHeight);
-    context.font = "small-caps 20px Impact";
-    context.fillStyle = "#FFF";
-    context.fillText("Score :   Lines :  Level : ", 0, context.height-2);
-    
-    //affichage des blocs déjà placés
-    for(let i = 0 ; i<10 ; i++){
-        for(let j = 0 ; j<20 ; j++){
-            if(grid[i][j]!=0){
-                context.fillStyle = color(grid[i][j]);
-                context.fillRect(i*tileWidth+1, j*tileHeight+1, tileWidth-2, tileHeight-2);
-            }
-        }
-    }
-    
-}
-
-/**
  * retourne le code Hexadécimal de la couleur d'un tetromino
  * @param {number} color Couleur du tetromino
  * @returns {String} Couleur en héxadécimale
@@ -325,42 +258,6 @@ color = function color(color){
             return "#00F";
     }
 }
-
-/**
- * Fonction de mise à jour de l'état du jeu
- * @param {number} d date 
- */
-update = function update(d) {
-    if(d-lastTimeUpdate > 700 || (downKeyDown && d-lastTimeUpdate > 100)){
-        tetromino.fall();
-        if(!tetromino.check()){
-            tetromino.upShift();
-            printInGrid();
-            tetromino = spawnTetromino();
-        }
-        lastTimeUpdate = d;
-    }
-    if(rightKeyDown && d-lastMoveTime > 100){
-        tetromino.rightShift();
-        if(!tetromino.check()){
-            tetromino.leftShift();
-        }
-        lastMoveTime = d;
-    }
-    if(leftKeyDown && d-lastMoveTime > 100){
-        tetromino.leftShift();
-        if(!tetromino.check()){
-            tetromino.rightShift();
-        }
-        lastMoveTime = d;
-    }
-    if(upKeyDown && d-lastMoveTime > 100){
-        tetromino = rotate(tetromino);
-        lastMoveTime = d;
-    }
-}
-
-/////////////////////////////
 
 /**
  * enregistre dans la grille la position du tetromino
@@ -593,6 +490,218 @@ rotate = function (tetromino) {
         }
     }
 }
+
+/**
+ * Fonction vérifiant si une ligne de la grille est complétée
+ * @param {Tetromino} tetromino tetromino qui s'est fixé sur la grille
+ * @param {int[][]} grid grille de jeu
+ * @returns {int[]} tableau des cases complétées
+ */
+completedLines = function(tetromino, grid){
+    var isCompleted = true;
+    var res = [];
+    var i = 0;
+    while (isCompleted && i<9){
+        if(grid [i][tetromino.block1.y] !=0){
+            i++;
+        }else{
+            isCompleted = false;
+        }
+    }
+    if(isCompleted){
+        res.push(tetromino.block1.y);
+    }
+    
+    if (!res.includes(tetromino.block2.y)){
+        i = 0; 
+        isCompleted = true;
+        while (isCompleted && i<9){
+            if(grid [i][tetromino.block2.y] !=0){
+                i++;
+            }else{
+                isCompleted = false;
+            }
+        }
+        if(isCompleted){
+            res.push(tetromino.block2.y);
+        }
+    }
+
+    if(!res.includes(tetromino.block3.y)){
+        i = 0; 
+        isCompleted = true;
+        while (isCompleted && i<9){
+            if(grid [i][tetromino.block3.y] !=0){
+                i++;
+            }else{
+                isCompleted = false;
+            }
+        }
+        if(isCompleted){
+            res.push(tetromino.block3.y);
+        }
+    }
+    
+    if(!res.includes(tetromino.block4.y)){
+        i = 0; 
+        isCompleted = true;
+        while (isCompleted && i<9){
+            if(grid [i][tetromino.block4.y] !=0){
+                i++;
+            }else{
+                isCompleted = false;
+            }
+        }
+        if(isCompleted){
+            res.push(tetromino.block4.y);
+        }
+    }
+    return res;
+}
+
+/**
+ * Action de descente d'une ligne de la grille de jeu après la suppression d'une ligne
+ * @param {number} y ordonnée de la ligne supprimée
+ * @param {number[][]} grid grille de jeu
+ */
+ lineFall = function(y, grid){
+    for (var i = 0; i < 10; i++){
+        grid [i][y] = grid [i][y-1];
+        grid [i][y-1] = 0;
+    }
+ }
+
+/**
+ * Action de suppression des lignes complétées
+ * @param {number[]} lines indices des lignes à supprimer
+ * @param {number[][]} grid grille de jeu
+ */
+deleteLines = function(lines, grid){
+    let y;
+    for (let i=0; i<lines.length; i++){
+        y = lines[i];
+        for (let x=0; x<9; x++){
+            grid[x][y] = 0;
+        }
+        for (let k=y; k>0; k--){
+            lineFall(k,grid);
+        }
+    }
+}
+
+
+/**
+ * Fonction d'initialisation/lancement du jeu
+ */
+init = function() {
+    context = document.getElementById("cvs").getContext("2d");
+    context.width = document.getElementById("cvs").width;
+    context.height = document.getElementById("cvs").height;
+    document.addEventListener("keydown", appuiClavier);
+    document.addEventListener("keyup", relacheClavier);
+    tileWidth = context.width/10;
+    tileHeight = (context.height-scoreWindowHeight)/20;
+    tetromino = spawnTetromino();
+    runGame();
+}
+/**
+ * Boucle de jeu
+ */
+runGame = function runGame() {
+    update(Date.now());
+    printStuff();
+    requestAnimationFrame(runGame);
+}
+
+/**
+ * Affichage des éléments suivants : 
+ * - grille de jeu (à finir)
+ * - tetromino en cours de placement
+ * - scores (à faire)
+ */
+printStuff = function printStuff() {
+    context.clearRect(0, 0, context.width, context.height);
+    
+    //affichage de la grille
+    context.fillStyle = "#555";
+    for(let i = 1; i<10;i++){
+        context.fillRect(tileWidth*i-1,0,2,context.height);
+    }
+    for(let i = 1;i<20;i++){
+        context.fillRect(0,tileHeight*i-1,context.width,2);
+    }
+
+    //affichage du tetromino
+    context.fillStyle = "#FFF"
+    context.fillRect(tetromino.block1.x*tileWidth, tetromino.block1.y*tileHeight, tileWidth, tileHeight);
+    context.fillRect(tetromino.block2.x*tileWidth, tetromino.block2.y*tileHeight, tileWidth, tileHeight);
+    context.fillRect(tetromino.block3.x*tileWidth, tetromino.block3.y*tileHeight, tileWidth, tileHeight);
+    context.fillRect(tetromino.block4.x*tileWidth, tetromino.block4.y*tileHeight, tileWidth, tileHeight);
+    context.fillStyle = color(tetromino.color);
+    context.fillRect(tetromino.block1.x*tileWidth+1, tetromino.block1.y*tileHeight+1, tileWidth-2, tileHeight-2);
+    context.fillRect(tetromino.block2.x*tileWidth+1, tetromino.block2.y*tileHeight+1, tileWidth-2, tileHeight-2);
+    context.fillRect(tetromino.block3.x*tileWidth+1, tetromino.block3.y*tileHeight+1, tileWidth-2, tileHeight-2);
+    context.fillRect(tetromino.block4.x*tileWidth+1, tetromino.block4.y*tileHeight+1, tileWidth-2, tileHeight-2);
+
+    //affichage du score
+    context.fillStyle = "#000";
+    context.fillRect(0,context.height-scoreWindowHeight,context.width,scoreWindowHeight);
+    context.font = "small-caps 20px Impact";
+    context.fillStyle = "#FFF";
+    context.fillText("Score :   Lines :  Level : ", 0, context.height-2);
+    
+    //affichage des blocs déjà placés
+    for(let i = 0 ; i<10 ; i++){
+        for(let j = 0 ; j<20 ; j++){
+            if(grid[i][j]!=0){
+                context.fillStyle = color(grid[i][j]);
+                context.fillRect(i*tileWidth+1, j*tileHeight+1, tileWidth-2, tileHeight-2);
+            }
+        }
+    }
+    
+}
+
+
+
+/**
+ * Fonction de mise à jour de l'état du jeu
+ * @param {number} d date 
+ */
+update = function update(d) {
+    if(d-lastTimeUpdate > 700 || (downKeyDown && d-lastTimeUpdate > 100)){
+        tetromino.fall();
+        if(!tetromino.check()){
+            tetromino.upShift();
+            printInGrid();
+            lines = completedLines(tetromino, grid);
+            if(lines.length !=0){
+                deleteLines(lines,grid);
+            }
+            tetromino = spawnTetromino();
+        }
+        lastTimeUpdate = d;
+    }
+    if(rightKeyDown && d-lastMoveTime > 100){
+        tetromino.rightShift();
+        if(!tetromino.check()){
+            tetromino.leftShift();
+        }
+        lastMoveTime = d;
+    }
+    if(leftKeyDown && d-lastMoveTime > 100){
+        tetromino.leftShift();
+        if(!tetromino.check()){
+            tetromino.rightShift();
+        }
+        lastMoveTime = d;
+    }
+    if(upKeyDown && d-lastMoveTime > 100){
+        tetromino = rotate(tetromino);
+        lastMoveTime = d;
+    }
+}
+
 
 appuiClavier = function appuiClavier(event){
     switch (event.keyCode){
